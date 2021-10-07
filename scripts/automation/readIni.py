eb0ee00baecd9cbcbc707069ca10734e6785cb40
Alexandre Mitsuru Kaihara <39930655+mdewinged@users.noreply.github.com>
2021-10-07 20:24:21 -0300

    ;
    .

--- scripts/automation/readIni.py
+++ scripts/automation/readIni.py
@@ -87,7 +87,7 @@ def getAndSetSubnetHostAndHostname(parser):
 # Downlaod recent server config 
 def getCurrentServerConfig():
 	newConfigFile = URLopener()
-	newConfigFile.retrieve("192.168.56.114/scripts/automation/packages/system/serverconfig.ini", "packages/system/serverconfig.ini")
+	newConfigFile.retrieve("192.168.56.123/scripts/automation/packages/system/serverconfig.ini", "packages/system/serverconfig.ini")
 	
 # Configure different server services 
 def configServers(parser, subnet, host):
@@ -171,7 +171,7 @@ def configServers(parser, subnet, host):
 	
 	# Configure Seafile (works only for linux, windows must be configured manually)
 	if platform.system() == "Linux":
-		cmd = "seaf-cli sync -l '" + seaFolder + "' -s  'http://" + seaIP + ":8000/' -d '/home/debian/sea' -u 'mininet' -p 'mininet' -c '/home/debian/.ccnet'"
+		cmd = "seaf-cli sync -l '" + seaFolder + "' -s  'http://" + seaIP + ":80/' -d '/home/debian/sea' -u 'alexandreamk1@gmail.com' -p 'Password123' -c '/home/debian/.ccnet'"
 		
 		cntErrors = 0
 		while cntErrors < 3:
@@ -187,7 +187,7 @@ def configServers(parser, subnet, host):
 def configMountWithOpenStackServer():
 	if platform.system() == "Linux":
 		try:
-			cmd = "mount -t cifs -o username=mininet,password=mininet //192.168.56.114/instancelogs /home/debian/log"
+			cmd = "mount -t cifs -o username=mininet,password=mininet //192.168.56.123/instancelogs /home/debian/log"
 			subprocess.check_call(cmd, shell=True)
 		except Exception as e:
 			echoC(__name__, "Mount log server error: " + str(e))
@@ -200,7 +200,7 @@ def configMountWithOpenStackServer():
 			echoC(__name__, "Unmount error: " + str(e))
 
 		try:
-			cmd = "net use M: \\\\192.168.56.114\\instancelogs"
+			cmd = "net use M: \\\\192.168.56.123\\instancelogs"
 			subprocess.check_call(cmd, shell=True)
 		except Exception as e:
 			echoC(__name__, "Mount log  server error: " + str(e))
