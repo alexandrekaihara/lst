3e0300cf734c5af63b13275c843a13952063298d
mdewinged <alexandreamk1@gmail.com>
2022-01-30 13:09:15 -0300

    .

--- network/internal/config_network.sh
+++ network/internal/config_network.sh
@@ -14,29 +14,26 @@
 #   - configure_host [container name] [subnet] [host]
 #   - configure_host mailserver 100 1
 configure_host(){
-## Add container to namespace. Available on: https://www.thegeekdiary.com/how-to-access-docker-containers-network-namespace-from-host/
-pid=$(docker inspect -f '{{.State.Pid}}' $1)
-mkdir -p /var/run/netns/
-ln -sfT /proc/$pid/ns/net /var/run/netns/$1
+    ## Add container to namespace. Available on: https://www.thegeekdiary.com/how-to-access-docker-containers-network-namespace-from-host/
+    pid=$(docker inspect -f '{{.State.Pid}}' $1)
+    mkdir -p /var/run/netns/
+    ln -sfT /proc/$pid/ns/net /var/run/netns/$1
 
-## Add interface on container and host
-ip link add veth$2.$3 type veth peer name vethsubnet$2
-ip link set veth$2.$3 up
+    ## Add interface on container and host
+    ip link add veth$2.$3 type veth peer name vethsubnet$2
+    ip link set veth$2.$3 up
 
-## Connect interfaces into the container subspace to the bridge
-ip link set vethsubnet$2 netns $1
-ip -n $1 link set vethsubnet$2 up
-ovs-vsctl add-port br-int veth$2.$3
+    ## Connect interfaces into the container subspace to the bridge
+    ip link set vethsubnet$2 netns $1
+    ip -n $1 link set vethsubnet$2 up
+    ovs-vsctl add-port br-int veth$2.$3
 
-## Add ip addressses and routes
-ip -n $1 addr add 192.168.$2.$3/16 dev vethsubnet$2
-ip netns exec $1 route add default gw 192.168.$2.100
+    ## Add ip addressses and routes
+    ip -n $1 addr add 192.168.$2.$3/16 dev vethsubnet$2
+    ip netns exec $1 route add default gw 192.168.$2.100
 
-# Define DNS server
-cat > /etc/resolv.conf << EOF
-nameserver 8.8.8.8
-nameserver 8.8.4.4
-EOF
+    # Define DNS server
+    ip netns exec $1 echo "nameserver 8.8.8.8" > /etc/resolv.conf
 }
 
 
@@ -59,11 +56,9 @@ ovs-vsctl set-controller br-int tcp:127.0.0.1:6633
 
 # Configure all hosts
 configure_host mailserver 100 1
-configure_host compassionate_lamport 100 1
 configure_host fileserver 100 2
 configure_host webserver 100 3
 configure_host backupserver 100 4
 configure_host Mlinuxclient1 200 2
 configure_host Olinuxclient1 210 2
 configure_host Dlinuxclient1 220 2
-
