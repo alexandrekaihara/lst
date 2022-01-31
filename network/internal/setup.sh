0cecdd49552a88f31e0f7b413c2b30a8d88d017e
mdewinged <alexandreamk1@gmail.com>
2022-01-31 12:10:14 -0300

    .

--- network/internal/setup.sh
+++ network/internal/setup.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
+# Remove all possible network configurations
+## Del bridge
+ovs-vsctl --if-exists del-br br-int
+## Delete all namespaces created
+rm -r /var/run/netns
 
 # Start Ryu Controller
 ryu-manager controller.py &
 
-
 # Set up all machines
 docker-compose --env-file=".env" up -d
-
 
 # Configure all networks
 chmod +x config_network.sh
