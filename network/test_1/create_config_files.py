0197d95f71ffdaf568f838865e958d7fb2248d06
mdewinged <alexandreamk1@gmail.com>
2022-02-03 16:12:47 -0300

    .

--- network/test_1/create_config_files.py
+++ network/test_1/create_config_files.py
@@ -60,12 +60,16 @@ class CreateConfigurationFiles():
         self.serverconfig = "[backup]\nip = " + d["100"][environ['REPOSITORY']+':'+environ["BACKUP"]] + 2*self.endl
         for subnet in subnets:
             self.serverconfig += '[' + subnet + ']' + self.endl
-            self.serverconfig += 'print = '   + d[subnet][environ['REPOSITORY']+':'+environ["PRINTER"]] + self.endl
-            self.serverconfig += 'mail = '    + d["100" ][environ['REPOSITORY']+':'+environ["MAILSERVER"]] + self.endl
-            self.serverconfig += 'file = '    + d["100" ][environ['REPOSITORY']+':'+environ["FILE"]] + self.endl
-            self.serverconfig += 'web = '     + d["100" ][environ['REPOSITORY']+':'+environ["WEB"]] + self.endl
+            if subnet != "50":
+                self.serverconfig += 'print = '   + d[subnet][environ['REPOSITORY']+':'+environ["PRINTER"]] + self.endl
+                self.serverconfig += 'mail = '    + d["100" ][environ['REPOSITORY']+':'+environ["MAILSERVER"]] + self.endl
+                self.serverconfig += 'file = '    + d["100" ][environ['REPOSITORY']+':'+environ["FILE"]] + self.endl
+                self.serverconfig += 'web = '     + d["100" ][environ['REPOSITORY']+':'+environ["WEB"]] + self.endl
+            else:
+                self.serverconfig += 'web = '     + d["50" ][environ['REPOSITORY']+':'+environ["WEB"]] + self.endl
             self.serverconfig += 'seafile = ' + d["50"  ][environ['REPOSITORY']+':'+environ["SEAFILE"]] + self.endl
             self.serverconfig += 'seafolder = '  + environ['SEAFOLDER'] + 2*self.endl
+        
         with(open(serverconfig_filename, "w")) as f:
             f.write(self.serverconfig)  
 
