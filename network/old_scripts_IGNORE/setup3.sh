6519fb0f9969a22259760190dfa1b531bec14ca5
mdewinged <alexandreamk1@gmail.com>
2022-02-05 11:17:33 -0300

    .

---
+++ network/old_scripts_IGNORE/setup3.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Remove all possible network configurations
+chmod +x tear_down_experiment.sh
+. tear_down_experiment.sh
+
+# Start Ryu Controller
+ryu-manager controller.py &
+
+# Set up all machines
+docker-compose --env-file=".env" up -d
+
+# Configure all networks
+chmod +x config_network.sh
+. config_network.sh
