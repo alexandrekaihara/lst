90be240d6af03f7ac99052df8a8991d6d91289e0
mdewinged <alexandreamk1@gmail.com>
2022-02-15 20:57:23 -0300

    .

--- experiment/controller.py
+++ experiment/controller.py
@@ -1,4 +1,21 @@
-# ruy-manager controller.py
+#
+# Copyright (C) 2022 Alexandre Mitsuru Kaihara
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+
 from ryu.base import app_manager
 from ryu.controller import ofp_event
 from ryu.controller.handler import DEAD_DISPATCHER, MAIN_DISPATCHER, CONFIG_DISPATCHER, HANDSHAKE_DISPATCHER, set_ev_cls
