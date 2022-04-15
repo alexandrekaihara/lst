f303994c06f7f18d35b560d45dd1a757ff85f193
Alexandre Mitsuru Kaihara <39930655+alexandrekaihara@users.noreply.github.com>
2022-04-14 22:29:55 -0300

    update repository name

--- docker/linux_client/onboot.sh
+++ docker/linux_client/onboot.sh
@@ -33,12 +33,12 @@ $ONBOOTLOG="${IP}_onboot.log"
 # Update the automation directory
 until unzip -o main.zip -d /home/debian
 do
-  wget https://github.com/alexandrekaihara/cidds/archive/refs/heads/main.zip --no-check-certificate
+  wget https://github.com/alexandrekaihara/lst/archive/refs/heads/main.zip --no-check-certificate
 done
 rm main.zip 
-mv /home/debian/cidds-main/scripts/automation /home/debian/
+mv /home/debian/lst-main/scripts/automation /home/debian/
 chmod -R 755 /home/debian/automation
-rm -r /home/debian/cidds-main
+rm -r /home/debian/lst-main
 
 # Move all configure files received
 mv /home/debian/printerip        /home/debian/automation/packages/system/printerip   
