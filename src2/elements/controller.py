a2ddfb69403ee85d38f96d86719d95bfc35349af
mdewinged <alexandreamk1@gmail.com>
2022-07-14 22:36:59 -0300

    ..

--- src2/elements/controller.py
+++ src2/elements/controller.py
@@ -14,136 +14,41 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
-from exceptions import MissingObjectParameter
 import logging
+import subprocess
+from elements.node import Node
+from exceptions import NodeInstantiationFailed
 
 
-# Brief: This class is responsible to configure the controllers of the experiment
-class Controller():
-    def __init__(self, object, name):
-        # Controller type parameters' names
-        self._IP = "controller_ip"
-        self._IMAGE = "image"
-        self._PORT = "controller_port"
-        self._COPY_FILE = "copy_file"
-        self._INSTANTIATE = "instantiate"
-        self._RUN = "run"    
+class Controller(Node):
+    def __init__(self, nodeName: str) -> None:
+        super(Controller, self).__init__(nodeName)
+        self.__process = 0
 
-        self._ip = self._set_ip(object, name)
-        self._image = self._set_image(object, name)
-        self._port = self._set_port(object, name)
-        self._copy_file = self._set_copy_file(object, name)
-        self._instantiate = self._set_instantiate(object, name)
-        self._run = self._set_run(object, name)
-
-    
-    # Brief: Set the ip parameter if exists
-    # Params:
-    #   Dict object: A link object
-    # Return:
-    #   Returns the value of the ip if exists, otherwise, returns none
-    def _set_ip(self, object, name):
-        try:
-            self._ip = object[self._IP]
-        except:
-            logging.error(f'The object {name} is missing the {self._IP} parameter definition')
-            raise MissingObjectParameter(f'The object {name} is missing the {self._IP} parameter definition')
-    
-    # Brief: Set the image parameter if exists
-    # Params:
-    #   Dict object: A link object
-    # Return:
-    #   Returns the value of the image if exists, otherwise, returns none
-    def _set_image(self, object, name):
-        try:
-            self._image = object[self._IMAGE]
-        except:
-            pass
-    
-    # Brief: Set the port parameter if exists
-    # Params:
-    #   Dict object: A link object
-    # Return:
-    #   Returns the value of the port if exists, otherwise, returns none
-    def _set_port(self, object, name):
-        try:
-            self._port = object[self._PORT]
-        except:
-            logging.error(f'The object {name} is missing the {self._PORT} parameter definition')
-            raise MissingObjectParameter(f'The object {name} is missing the {self._PORT} parameter definition')
-    
-    # Brief: Set the copy_file parameter if exists
-    # Params:
-    #   Dict object: A link object
-    # Return:
-    #   Returns the value of the copy_file if exists, otherwise, returns none
-    def _set_copy_file(self, object, name):
-        try:
-            self._copy_file = object[self._COPY_FILE]
-        except:
-            pass
-
-    # Brief: Set the instantiate parameter if exists
-    # Params:
-    #   Dict object: A link object
-    # Return:
-    #   Returns the value of the instantiate if exists, otherwise, returns none
-    def _set_instantiate(self, object, name):
-        try:
-            self._instantiate = object[self._INSTANTIATE]
-        except:
-            logging.error(f'The object {name} is missing the {self._INSTANTIATE} parameter definition')
-            raise MissingObjectParameter(f'The object {name} is missing the {self._INSTANTIATE} parameter definition')
-    
-    # Brief: Set the run parameter if exists
-    # Params:
-    #   Dict object: A link object
-    # Return:
-    #   Returns the value of the run if exists, otherwise, returns none
-    def _set_run(self, object, name):
-        try:
-            self._run = object[self._RUN]
-        except:
-            pass
+    def instantiate(self, controllerIp, controllerPort):
+        process = self.__getProcess()
+        if process == 0:
+            try:
+                self.__process = subprocess.Popen(f"ryu-manager --ofp-listen-host={controllerIp} --ofp-tcp-listen-port={controllerPort} controller.py > controller.log", shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+            except Exception as ex:
+                logging.error(f"Error while creating the switch {self.getNodeName()}: {str(ex)}")
+                raise NodeInstantiationFailed(f"Error while creating the switch {self.getNodeName()}: {str(ex)}")
+        else:
+            logging.error(f"Controller {self.getNodeName()} already instantiated")
+            raise Exception(f"Controller {self.getNodeName()} already instantiated")
         
-    # Brief: Returns the value of ip
-    # Params:
-    # Return:
-    #   Returns the value of the ip
-    def get_ip(self):
-        return self._ip
-
-    # Brief: Returns the value of image
-    # Params:
-    # Return:
-    #   Returns the value of the image
-    def get_image(self):
-        return self._image
-
-    # Brief: Returns the value of port
-    # Params:
-    # Return:
-    #   Returns the value of the port
-    def get_port(self):
-        return self._port
-
-    # Brief: Returns the value of copy_file
-    # Params:
-    # Return:
-    #   Returns the value of the copy_file
-    def get_copy_file(self):
-        return self._copy_file
-
-    # Brief: Returns the value of instantiate
-    # Params:
-    # Return:
-    #   Returns the value of the instantiate
-    def get_instantiate(self):
-        return self._instantiate
-
-    # Brief: Returns the value of run
-    # Params:
-    # Return:
-    #   Returns the value of the run
-    def get_run(self):
-        return self._run
+    def delete(self):
+        process = self.__getProcess()
+        if process != 0:
+            try:
+                self.__process.kill()
+                _, stderr = self.__process.communicate()
+            except Exception as ex:
+                logging.error(f"Error while deleting the switch {self.getNodeName()}: {str(ex)}\nThreads error: {stderr}")
+                raise NodeInstantiationFailed(f"Error while deleting the switch {self.getNodeName()}: {str(ex)}\nThreads error: {stderr}")
+        else:
+            logging.error(f"Can't delete {self.getNodename()}. {self.getNodename()} was not instantiated.")
+            raise Exception(f"Can't delete {self.getNodename()}. {self.getNodename()} was not instantiated.")
+        
+    def __getProcess(self):
+        return self.__process
