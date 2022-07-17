9a9233104d8a2a377f3f10a1850610a180aed7f9
mdewinged <alexandreamk1@gmail.com>
2022-07-16 21:30:46 -0300

    .'

--- src2/elements/node.py
+++ src2/elements/node.py
@@ -39,7 +39,7 @@ class Node:
     def instantiate(self, dockerImage="ubuntu:20.04", dockerCommand = '') -> None:
         try:    
             if dockerCommand == '':
-                subprocess.run(f"docker run -d --network=none --name={self.getNodeName()} {dockerImage}", shell=True)
+                subprocess.run(f"docker run -d --network=none --name={self.getNodeName()} {dockerImage} tail -f /dev/null", shell=True)
                 self.__enableNamespace(self.getNodeName())
             else:
                 subprocess.run(dockerCommand, shell=True)
