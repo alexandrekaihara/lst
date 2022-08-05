f621c330ee5f33bf3d4cec526584b5ab0e2f4732
mdewinged <alexandreamk1@gmail.com>
2022-08-05 20:08:12 -0300

    '.'

--- src/switch.py
+++ src/switch.py
@@ -137,7 +137,7 @@ class Switch(Node):
 
     def __collectFlows(self, node: Node) -> None:
         try:
-            subprocess.run(f"sudo ./TCPDUMP_and_CICFlowMeter/capture_interface_pcap.sh {self.__getThisInterfaceName(node)} /TCPDUMP_and_CICFlowMeter/collecteddata", shell=True)
+            subprocess.run(f"sudo ./TCPDUMP_and_CICFlowMeter/capture_interface_pcap.sh {self._Node__getThisInterfaceName(node)} /TCPDUMP_and_CICFlowMeter/collecteddata", shell=True)
         except Exception as ex:
             logging.error(f"Error set the collector on {self.getNodeName()} to {node.getNodeName()}: {str(ex)}")
             raise Exception(f"Error set the collector on {self.getNodeName()} to {node.getNodeName()}: {str(ex)}")
